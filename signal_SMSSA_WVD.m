280320d140f40189e0a4f39165a002d7bc9b7388
Rajesh Tripathy <rajesh.nitr11@gmail.com>
2021-01-23 12:32:29 +0530

    Update signal_SMSSA_WVD.m

--- signal_SMSSA_WVD.m
+++ signal_SMSSA_WVD.m
@@ -7,14 +7,7 @@ close all;
 %%Circuits, Systems, and Signal Processing (2020): 1-26.
 %%Siddharth, T., Tripathy, R. K., & Pachori, R. B. (2019). Discrimination of focal and non-focal seizures from EEG signals using sliding mode singular spectrum analysis. IEEE Sensors Journal, 19(24), 12286-12296.
 %%Harmouche, J., Fourer, D., Auger, F., Borgnat, P., & Flandrin, P. (2017). The sliding singular spectrum analysis: A data-driven nonstationary signal decomposition tool. IEEE Transactions on Signal Processing, 66(1), 251-263.
-%%The ssa_decomp (autossa) function has been taken from the following
-%%papers. Make sure, you should also cite the following two papers if you
-%%use these codes
-%%% Refs: 
-% [J. Harmouche, D. Fourer, P. Flandrin, F. Auger and P. Borgnat. One
-% or Two Components ? The Singular Spectrum Analysis answers. Proc. SLRA'2015. Grenoble, France.June 2015]
-% [J. Harmouche, D. Fourer, P. Flandrin, F. Auger and P. Borgnat. Une ou deux composantes:
-% la réponse de l'analyse spectrale singulière. Proc. GRETSI'15. Lyon, France]
+%%%%ssa_decomp and ssa functions has been taken from https://codeocean.com/capsule/5444528/tree/v1 %%%%% before running this MATLAB file, you should downlaod both files and add these files into the MATLAB path
 
 
 %% Signal generation
